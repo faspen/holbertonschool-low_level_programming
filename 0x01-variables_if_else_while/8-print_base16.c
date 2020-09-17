92864c2d532ff6216fab595fda18187ad5d7f017
faspen <aspensonf@gmail.com>
2020-09-17 23:23:53 +0000

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0; Success
+*/
+
+int main(void)
+{
+int num;
+
+scanf("%x", &num);
+printf("%x\n", num);
+printf("%d\n", num);
+
+return (0);
+}
