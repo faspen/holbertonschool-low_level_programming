10ceefba625ad20d00f4417c69f2411abef265b9
faspen <aspensonf@gmail.com>
2020-09-22 15:02:53 +0000

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,21 @@
 #include "holberton.h"
 /**
-  * print_alphabet_x10 - entry point
+  * print_alphabet_x10 - Entry point
+  *
+  * Return: Always 0; Success
+  */
 
+void print_alphabet_x10(void)
+{
+	int b;
+	int r;
+
+	for (r = 1; r <= 10; r++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
