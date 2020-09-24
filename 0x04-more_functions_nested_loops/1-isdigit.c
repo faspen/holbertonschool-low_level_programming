36e275b6726f3efc15234362d332b960fe9c1f39
faspen <aspensonf@gmail.com>
2020-09-24 15:18:54 +0000

    stuff

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+  * _isdigit - Entry
+  *
+  * @c: integer
+  *
+  * Return: 1 or 0;
+  */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  * mul - entry
+  *
+  * @a: integer
+  *
+  * @b: integer
+  *
+  * Return: result
+  */
+
+int mul(int a, int b)
+{
+	int n;
+
+	n = a * b;
+	return (n);
+}
