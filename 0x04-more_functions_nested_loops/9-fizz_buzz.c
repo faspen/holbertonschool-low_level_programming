852e8ff3b10d6db13dd9a24945be3a8d7e7c0e0a
faspen <aspensonf@gmail.com>
2020-09-25 19:42:59 +0000

    buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+  * main - Entry
+  * Return: 0
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i < 100)
+		{
+			putchar(' ');
+		}
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((i % 3 && i % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
