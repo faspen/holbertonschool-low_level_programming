a32348a60eee08cc9b37eda3d6b0e11e5d3f76ca
faspen <aspensonf@gmail.com>
2020-09-25 00:24:40 +0000

    fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,16 +10,19 @@ void print_square(int size)
 	int r;
 	int c;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
 	for (r = 1; r <= size; r++)
 	{
 		for (c = 1; c <= size; c++)
 		{
-			if (size <= 0)
-			{
-				_putchar('\n');
-			}
 			_putchar('#');
 		}
 		_putchar('\n');
+	}
 	}
 }
