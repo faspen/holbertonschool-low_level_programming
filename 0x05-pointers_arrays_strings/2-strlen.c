fa7142403b12cd04f42949bb9819610d5653fdb0
faspen <aspensonf@gmail.com>
2020-09-28 20:33:07 +0000

    fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,14 @@
 /**
   * _strlen - Entry
   * @s: string
+  * Return: length
   */
 
 int _strlen(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++);
+	for (i = 0; s[i] != '\0'; i++)
 
 	return (i);
 }
