455edf5a6fb23968a1356d52d8e8c6e58e0f4dd5
faspen <aspensonf@gmail.com>
2020-09-29 19:45:03 +0000

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+  * puts_half - Entry
+  * @str: string
+  */
+
+void puts_half(char *str)
+{
+	int i;
+
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		;
+	}
+	if (len % 2 == 0)
+	{
+		i = len / 2;
+	}
+	else
+	{
+		i = (len + 1) / 2;
+	}
+	for (; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	_putchar('\n');
+}
