b7574f3e7eedcd2e2f15c5a87b15e058705048c9
faspen <aspensonf@gmail.com>
2020-09-29 15:38:56 +0000

    fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 /**
+  * rev_string - Entry
   *
+  * @s: string
   */
 
 void rev_string(char *s)
 {
 	int i;
-	
+
 	int j;
-	
+
 	int len;
 
 	for (len = 0; s[len] != '\0'; len++)
@@ -19,8 +21,9 @@ void rev_string(char *s)
 	for (i = 0, j = len - 1; i < j; i++, j--)
 	{
 		char ch = s[i];
+
 		s[i] = s[j];
+
 		s[j] = ch;
 	}
 }
-	
