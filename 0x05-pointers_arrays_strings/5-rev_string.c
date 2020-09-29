7f8220c033fe9032f8c77f03113b8897bbf65b7a
faspen <aspensonf@gmail.com>
2020-09-29 15:36:58 +0000

    esrever

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+  *
+  */
+
+void rev_string(char *s)
+{
+	int i;
+	
+	int j;
+	
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+		;
+	}
+
+	for (i = 0, j = len - 1; i < j; i++, j--)
+	{
+		char ch = s[i];
+		s[i] = s[j];
+		s[j] = ch;
+	}
+}
+	
