aeaecd128a64db8b4803aa9805b4f7e1272f4446
faspen <aspensonf@gmail.com>
2020-10-02 16:26:21 +0000

    pld

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+char *rot13(char *s)
+{
+	unsigned int i, j;
+	char arr1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char arr2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		for (j = 0; arr1[j] != '\0'; ++j)
+		{
+			if (s[i] == arr1[j])
+			{
+				s[i] = arr2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
