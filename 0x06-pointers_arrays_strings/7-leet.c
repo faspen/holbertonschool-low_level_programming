7cfca0e17d9ad5d987356775e5cf79b3fdb7b125
faspen <aspensonf@gmail.com>
2020-10-02 16:20:25 +0000

    pld

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  *
+  */
+char *leet(char *str)
+{
+	char *let = "aAeEoOtTlL", *num = "43071";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; let[j] != '\0'; j++)
+		{
+			if (str[i] == let[j])
+				str[i] = num[j / 2];
+		}
+	}
+	return (str);
+}
