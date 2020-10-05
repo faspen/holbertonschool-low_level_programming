a28f727d39fa3c363ff6dd2d3923270a3169bf96
faspen <aspensonf@gmail.com>
2020-10-05 20:17:48 +0000

    haha

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,11 +12,14 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int mem;
+	char *s2;
+
+	s2 = s;
 
 	for (mem = 0; mem < n; mem++)
 	{
 		*s = b;
 		s++;
 	}
-	return (s);
+	return (s2);
 }
