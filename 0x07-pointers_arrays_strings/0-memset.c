c6355ae0bc4a190823b2565134f72a39c33bb0fe
faspen <aspensonf@gmail.com>
2020-10-05 18:47:18 +0000

    guessing game

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+  * _memset - Entry
+  *
+  * @s: memory area
+  * @b: constant
+  * @n: first characters
+  *
+  * Return: new string
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int mem;
+
+	for (mem = 0; mem < n; mem++)
+	{
+		*s = b;
+		s++;
+	}
+	return (s);
+}
