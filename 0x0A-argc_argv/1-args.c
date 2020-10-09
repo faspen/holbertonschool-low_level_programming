d2b21812f5f56d25da8316adb9ee5d29937194c0
faspen <aspensonf@gmail.com>
2020-10-09 15:19:14 +0000

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+  * main - entry
+  *
+  * @argc: count
+  * @argv: vector
+  *
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	argv = argv;
+	return (0);
+}
