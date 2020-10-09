07454ba676157f74990ce47266afaa3021c89f1a
faspen <aspensonf@gmail.com>
2020-10-09 15:09:41 +0000

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include "holberton.h"
 /**
+  * main - entry
   *
+  * @argc: count
+  * @argv: vector
+  *
+  * Return: 0
   */
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
