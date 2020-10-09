06ab548dacb1dd6012afe2d5f3cbadef8821bd84
faspen <aspensonf@gmail.com>
2020-10-09 15:29:09 +0000

    betty

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
+  * main - entry
   *
+  * @argc: count
+  * @argv: vector
+  *
+  * Return: 0
   */
 
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
