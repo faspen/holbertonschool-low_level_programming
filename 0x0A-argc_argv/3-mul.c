42e3fbc65c476b7f5077e4fbf1195f77b66fe0ce
faspen <aspensonf@gmail.com>
2020-10-09 18:37:16 +0000

    add

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  *
+  */
+
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		if (argc > 3)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", mul);
+	return (0);
+}
