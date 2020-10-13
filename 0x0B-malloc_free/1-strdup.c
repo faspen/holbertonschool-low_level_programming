d9ddd2589f6748ae0282dce2fe44e3a0897c2b81
faspen <aspensonf@gmail.com>
2020-10-13 18:42:41 +0000

    dup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * _strdup - return pointer to duplicate of str
+  *
+  * @str: duplicated string
+  *
+  * Return: pointer to new string
+  */
+
+char *_strdup(char *str)
+{
+	char *new;
+
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(char) * 10);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		*(new + i) = *(str + i);
+	}
+	return (new);
+}
