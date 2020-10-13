6c8dd3e5ec4dae6db3b12b796b0bcd8767cbfc47
faspen <aspensonf@gmail.com>
2020-10-13 20:27:00 +0000

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,6 +32,14 @@ char *str_concat(char *s1, char *s2)
 
 	char *cat = malloc(sizeof(char) * len);
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	if (cat == NULL)
 	{
 		return (NULL);
