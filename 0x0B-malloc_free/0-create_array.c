4e5500f85641486cd1dfe4aafaa7955e042e1bcb
faspen <aspensonf@gmail.com>
2020-10-13 18:13:02 +0000

    fix

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * create_array - create array
+  *
+  * @size: array size
+  * @c: character in string
+  *
+  * Return: pointer to string
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int mem;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(sizeof(c) * size);
+
+	if (arr == 0)
+	{
+		return (NULL);
+	}
+	for (mem = 0; mem < size; mem++)
+	{
+	}
+	arr[mem] = c;
+	return (arr);
+}
