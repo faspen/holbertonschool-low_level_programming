85a6bb11258e3bfe0dc68b428f9371e71804da95
faspen <aspensonf@gmail.com>
2020-10-14 16:06:35 +0000

    pld

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  *
+  */
+
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	for (x = 0; x < height; x++)
+		free(grid[x]);
+
+	free(grid);
+}
