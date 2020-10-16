2f7011ba5b3856f377252fe937ddcb6d9843923c
faspen <aspensonf@gmail.com>
2020-10-16 14:35:28 +0000

    pld

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,19 +12,21 @@
 int *array_range(int min, int max)
 {
 	int *arr;
-	int i = 0, tempmin;
+	int i = 0, tempmin, total;
+
+	total = max - min;
 
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * ((max - min) + 1));
+	arr = malloc(sizeof(int) * (total + 1));
 
 	if (arr == NULL)
 		return (NULL);
 
 	tempmin = min;
 
-	while (i <= (max - min))
+	while (i <= (total + 1))
 	{
 		i++;
 		tempmin++;
