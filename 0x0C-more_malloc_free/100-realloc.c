3518d2d34e31554449dd687baac21a3224ab25ea
faspen <aspensonf@gmail.com>
2020-10-16 18:16:26 +0000

    pld

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  *
+  */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *voiid;
+	char *arr;
+	char *temp = ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	arr = malloc(new_size);
+
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < new_size && i < old_size; i++)
+	{
+		arr[i] = temp[i];
+	}
+	voiid = arr;
+	free(ptr);
+	return (voiid);
+}
