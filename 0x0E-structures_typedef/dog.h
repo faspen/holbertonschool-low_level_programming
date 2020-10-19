1831169db02627156dfbcf5bb556cd2852f199e7
faspen <2335@holbertonschool.com>
2020-10-19 20:16:01 +0000

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,20 @@
-int _putchar(char c);
+#ifndef DOGGY_H
+#define DOGGY_H
+
+/**
+  * struct dog - struct
+  * @name: name
+  * @age: age
+  * @owner: owner
+  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+int _putchar(char c);
+
+#endif
