cd25d531c3a014b225101ecba15e6e2c19867acd
faspen <2335@holbertonschool.com>
2020-10-21 15:37:07 +0000

    0-print_name.c and readme

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+  * print_name - print a name
+  *
+  * @name: given name
+  * @f: pointer
+  *
+  * Return: nothing
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
