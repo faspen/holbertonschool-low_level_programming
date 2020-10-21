09dd914648969cb4d0828da192953a0ff347da48
faspen <2335@holbertonschool.com>
2020-10-21 18:52:31 +0000

    indents? idk betty is weird

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 /**
+  * int_index - entry
   *
+  * @array: array
+  * @size: size
+  * @cmp: function
+  *
+  * Return: -1
   */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -8,17 +14,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int len;
 
 	if (size <= 0)
-		return (-1);
-
-	if (cmp != NULL && array != NULL)
 	{
-		for (len = 0; len < size; len++)
+		return (-1);
+	}
+
+if (cmp != NULL && array != NULL)
+{
+	for (len = 0; len < size; len++)
+	{
+		if (cmp(array[len]) != 0)
 		{
-		       if (cmp(array[len]) != 0)
-		       {
-			       return (len);
-		       }
+			return (len);
 		}
 	}
-	return (-1);
+}
+return (-1);
 }
