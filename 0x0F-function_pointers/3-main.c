cfaed5c84d83f6b4e951dfaa0ef84b6ec414f74b
faspen <2335@holbertonschool.com>
2020-10-22 15:39:54 +0000

    QUICK MATHS

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+  * main - math
+  *
+  * @argc: number of
+  * @argv: arguments
+  *
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int (*op_func)(int, int);
+	int num1, num2, result;
+
+	if (argc < 4 || argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
