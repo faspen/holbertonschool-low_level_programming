f58187acbf2737303d4b1134d96ad84c247af0cb
faspen <2335@holbertonschool.com>
2020-11-03 18:59:09 +0000

    betty fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 /**
+  * add_nodeint_end - add node to end
   *
+  * @head: top
+  * @n: int
+  *
+  * Return: end node
   */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
