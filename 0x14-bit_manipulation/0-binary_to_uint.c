6a05da5c9af9f19523a8b5f8a41333163187e1dd
faspen <2335@holbertonschool.com>
2020-11-05 17:57:13 +0000

    helper function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,26 @@
 #include "holberton.h"
+
+/**
+  * _strlen - get length
+  * @s: string
+  * Return: length
+  */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		;
+	}
+	return (i);
+}
+
 /**
   * binary_to_uint - from binary to unsigned int
   * @b: number to be converted
   * Return: result
   */
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int j = 1;
@@ -15,7 +31,7 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	len = strlen(b);
+	len = _strlen(b);
 
 	for (c = len - 1; c >= 0; c--)
 	{
