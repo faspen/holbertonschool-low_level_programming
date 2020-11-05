4fbc83344a1abefbc0e953c0dc8451e2ff71ada9
faspen <2335@holbertonschool.com>
2020-11-05 22:35:31 +0000

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+  * set_bit - set bit at index to 0
+  * @n: int
+  * @index: index
+  * Return: 1 on success, -1 on fail
+  */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (n == NULL)
+		return (-1);
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	i = 1 << index;
+	*n = *n & ~i;
+	return (1);
+}
