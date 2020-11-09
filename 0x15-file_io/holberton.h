f8519e208f1a8cd856bd89f8b2bc8897c4b857cf
faspen <2335@holbertonschool.com>
2020-11-09 16:28:39 +0000

    new proj

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLB_H
+#define HOLB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#endif
