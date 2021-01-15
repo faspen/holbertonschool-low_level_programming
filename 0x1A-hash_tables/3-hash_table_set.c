80eceeacf7bf01b4f3c565d6dc4c6b67ba52baa6
faspen <aspensonf@gmail.com>
2021-01-15 01:22:55 +0000

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!value || !key || !ht || !strcmp(key, ""))
 		return (0);
-	
+
 	index = key_index((unsigned char *)key, ht->size);
 	node = ht->array[index];
 
